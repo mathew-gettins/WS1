b0ca4b0b4f97f2a6f6539e13d8ed79c1bf910316
S00240421 <S00240421@LAB-D2038-09>
2023-01-30 09:23:51 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintIntegersfor();
+            PrintIntegersWhile();
+            PrintIntegersDo();
+            PrintExcudingFor();
+            PrintDecendingFor();    
+        }
+
+        static void PrintIntegersfor()
+        {
+            for (int i = 40; i <= 60; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        static void PrintIntegersWhile()
+        {
+            int i = 40;
+            while (i <= 60)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+        }
+
+        static void PrintIntegersDo()
+        {
+            int i = 40;
+            do
+            {
+                Console.WriteLine(i);
+                i++;
+            } while (i <= 60);
+        }
+        static void PrintExcudingFor()
+        {
+            for (int i = 40; i <= 60; i++)
+                if (i != 46 && i != 48)
+                {
+                    Console.WriteLine();
+                    i++;
+                }
+        }
+
+        static void PrintDecendingFor()
+        {
+            for (int i = 60; i >= 40; i--)
+               
+                {
+                    Console.WriteLine(i);
+                }
+        }
+    }
+}
